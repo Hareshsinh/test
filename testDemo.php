fe0aadaccc905590ed55ae813ef5a1ddc00fafc1
Hareshsinh <haresh.sisodiya@viitorcloud.com>
2017-02-03 16:44:53 +0530

    Add files via upload

---
+++ testDemo.php
@@ -0,0 +1,35 @@
+<?php
+	echo "dsds";
+?>
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>jQuery.getJSON demo</title>
+  <style>
+  img {
+    height: 100px;
+    float: left;
+  }
+  </style>
+  <script src="https://code.jquery.com/jquery-1.10.2.js"></script>
+</head>
+<body>
+ 
+<div id="images"></div>
+ 
+<script>
+
+(function() {
+  alert('dsd');
+  // var flickerAPI = "http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
+  var flickerAPI = "test.php?jsoncallback=?";
+  $.getJSON( flickerAPI, {test:'test'})
+    .done(function(data) {
+      alert(data);
+    });
+})();
+</script>
+ 
+</body>
+</html>
